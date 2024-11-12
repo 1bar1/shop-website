eb82284c134038df9c385b0f1639c3b59f085fb4
1bar1 <sontbar@gmail.com>
2024-11-13 01:05:56 +0200

    hero img change, hero button change

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
     <div className='flex flex-col sm:flex-row' style={{ backgroundColor: 'rgb(255, 218, 214)' }}>
       {/*-------LEFT SIDE---------*/}
       <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-        <div className='text-[#414141]'>
+        <div className='text-[#414141] flex flex-col items-start justify-center gap-y-4'>
           <div className='flex items-center gap-2'>
             <p className='w-6 md:w-8 h-[2px] bg-[#414141]'></p>
             <p className='font-medium text-sm md:text-base'>WE ARE FOR YOU</p>
           </div>
           <h1 className='prata-regular text-5xl sm:py-3 lg:text-7xl leading-relaxed text-black'>Mishi, it's simple.</h1>
-          <div className='flex items-center gap-2 mt-4'>
+          <div className='sm:mt-4 self-center'>
             <NavLink to='/collection'>
-            <button className='bg-[#414141] text-white font-semibold text-sm md:text-base py-2 px-5 rounded-lg shadow-lg hover:bg-[#333] transition-colors duration-300'>SHOP NOW</button>
+              <button className='bg-[#414141] text-white font-semibold text-sm md:text-base py-2 px-5 rounded-lg shadow-lg bg-black hover:bg-[#333] transition-colors duration-300'>SHOP NOW</button>
             </NavLink>
           </div>
         </div>
